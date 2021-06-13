b0d59cc91d0451a7589d647ef0ed6779de90be6e
dawid <caushered@gmail.com>
2021-06-13 12:06:31 +0200

    encrypt with two keys

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -27,6 +27,45 @@ public class CaesarCipher {
         return encrypted.toString();
     }
 
+    public String encryptTwoKeys(String input, int key1, int key2) {
+        //key1 encrypts letters which are located on ODD positions in input
+        //key2 encrypts letters which are located on EVEN positions in input
+        StringBuilder encrypted = new StringBuilder(input);
+        StringBuilder encryptedOdd = new StringBuilder();
+        StringBuilder encryptedEven = new StringBuilder();
+        //create two stringbuilders based on location (odd or even) in provided string
+        for (int i = 0; i < encrypted.length(); i += 2) {
+            encryptedOdd.append(encrypted.charAt(i));
+        }
+        for (int i = 1; i < encrypted.length(); i += 2) {
+            encryptedEven.append(encrypted.charAt(i));
+        }
+        //encrypt both strings with appropriate key
+        encryptedOdd = new StringBuilder(encrypt(encryptedOdd.toString(), key1));
+        encryptedEven = new StringBuilder(encrypt(encryptedEven.toString(), key2));
+
+        //create whole encrypted stringbuilder
+        int index = 0;
+        while (true) {
+            if (index >= encryptedOdd.length()) {
+                break;
+            }
+            int temp = index * 2;
+            char oddChar = encryptedOdd.charAt(index);
+            encrypted.setCharAt(temp, oddChar);
+            if (index >= encryptedEven.length()) {
+                break;
+            }
+            char evenChar = encryptedEven.charAt(index);
+            encrypted.setCharAt(temp + 1, evenChar);
+            index++;
+        }
+
+        return encrypted.toString();
+    }
+    //==============================================
+    //=================TESTING======================
+    //==============================================
     public void testEncrypt() {
         String msg = "There is a cake in Your room.";
         int key = 3;
@@ -34,8 +73,18 @@ public class CaesarCipher {
         System.out.println("[ENCRYPTED] " + encrypted);
         System.out.println("[DECRYPTED] " + encrypt(encrypted, 26 - key));
         msg = "TRYING to make enCRYPTEd msG with SOME MYSTERIOUS keY.";
-        key = 27;
+        key = 21;
+        encrypted = encrypt(msg, key);
         System.out.println("[ENCRYPTED] " + encrypted);
         System.out.println("[DECRYPTED] " + encrypt(encrypted, 26 - key));
+    }
+
+    public void testEncryptTwoKeys() {
+        String msg = "First Legion";
+        int key1 = 23;
+        int key2 = 17;
+        String encrypted = encryptTwoKeys(msg, key1, key2);
+        System.out.println("[ENCRYPTED] " + encrypted);
+        System.out.println("[DECRYPTED] " + encryptTwoKeys(encrypted, 26 - key1, 26 - key2));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         CaesarCipher cc = new CaesarCipher();
-        cc.testEncrypt();
+        //cc.testEncrypt();
+        cc.testEncryptTwoKeys();
     }
 }
